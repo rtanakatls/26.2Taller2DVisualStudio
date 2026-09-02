67736dcf70292cb1d2f8a5004ac48ff3c69c3522
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2026-09-02 17:37:57 -0500

    Created project

---
+++ Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2D262
+{
+    internal class Menu
+    {
+        public void Execute()
+        {
+            string name;
+            Console.WriteLine("Hola!");
+            name = GetName();
+            Console.WriteLine($"Veo que te llamas {name}");
+        }
+
+        private string GetName()
+        {
+            string name="";
+            while (name.Length == 0)
+            {
+                Console.WriteLine("Introduce tu nombre:");
+                name = Console.ReadLine();
+                if(name.Length==0)
+                {
+                    Console.WriteLine("El nombre no puede ser vacío");
+                }
+            }
+            return name;
+        }
+    }
+}
