42b665bc6599159a6dfed7265e9ab6860d15bde6
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2026-09-09 16:11:22 -0500

    Added files

---
+++ Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taller2D262
+{
+    internal class Person
+    {
+        private string name;
+        private int damage;
+        private int hp;
+        public string Name { get { return name; } } 
+
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            hp -= damage;
+        }
+    }
+}
