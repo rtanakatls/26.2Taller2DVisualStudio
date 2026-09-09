a8892627ebccbea738d9c2fad4ddf4d833ce25c0
Ricardo Tanaka <rtanaka@talento.tls.edu.pe>
2026-09-09 16:32:14 -0500

    Changed constructor

--- Player.cs
+++ Player.cs
@@ -15,9 +15,10 @@ namespace Taller2D262
 
         private int experience;
 
-        public Player(string name, int hp, int level)
+        public Player(string name,int id, int hp, int level)
         {
             this.name = name;
+            this.id = id;
             this.hp = hp;
             this.level = level;
         }
